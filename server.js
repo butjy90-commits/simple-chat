38ba98f863fa17f7f943c8a0695edf51cf03aeb9
butjy90-commits <butjy90@gmail.com>
2026-03-09 12:49:45 +0000

    Add colors, rooms, typing indicator, and message bubbles

--- server.js
+++ server.js
@@ -8,48 +8,52 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let users = {};
+const users = {};      // { socket.id: {name, color, room} }
+const colors = ["#e6194b","#3cb44b","#ffe119","#4363d8","#f58231","#911eb4","#46f0f0","#f032e6","#bcf60c","#fabebe"];
 
 io.on("connection", (socket) => {
 
-    socket.on("join", (username) => {
+  // User joins a room
+  socket.on("join", ({username, room}) => {
 
-        users[socket.id] = username;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    users[socket.id] = { name: username, color, room };
+    socket.join(room);
 
-        io.emit("system message", username + " joined the chat");
+    io.to(room).emit("system message", `${username} joined ${room}`);
+  });
 
+  // Chat message
+  socket.on("chat message", (msg) => {
+    const user = users[socket.id];
+    if(!user) return;
+
+    const time = new Date().toLocaleTimeString();
+    io.to(user.room).emit("chat message", {
+      user: user.name,
+      text: msg,
+      time,
+      color: user.color
     });
+  });
 
-    socket.on("chat message", (msg) => {
+  // Typing
+  socket.on("typing", () => {
+    const user = users[socket.id];
+    if(user) socket.to(user.room).emit("typing", user.name);
+  });
 
-        const username = users[socket.id] || "Unknown";
-
-        const time = new Date().toLocaleTimeString();
-
-        io.emit("chat message", {
-            user: username,
-            text: msg,
-            time: time
-        });
-
-    });
-
-    socket.on("disconnect", () => {
-
-        const username = users[socket.id];
-
-        if(username){
-            io.emit("system message", username + " left the chat");
-        }
-
-        delete users[socket.id];
-
-    });
+  socket.on("disconnect", () => {
+    const user = users[socket.id];
+    if(user) {
+      io.to(user.room).emit("system message", `${user.name} left ${user.room}`);
+      delete users[socket.id];
+    }
+  });
 
 });
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
